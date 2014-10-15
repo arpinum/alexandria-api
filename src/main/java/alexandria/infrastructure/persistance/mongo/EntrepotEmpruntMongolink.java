1f3dfec2fb0e449f04a3b792666092f4e9343dbd
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2014-10-15 16:16:05 +0200

    refacto pour utiliser mongodb pour compter

--- src/main/java/alexandria/infrastructure/persistance/mongo/EntrepotEmpruntMongolink.java
+++ src/main/java/alexandria/infrastructure/persistance/mongo/EntrepotEmpruntMongolink.java
@@ -4,11 +4,8 @@ import alexandria.modele.bibliotheque.Exemplaire;
 import alexandria.modele.emprunt.Emprunt;
 import alexandria.modele.emprunt.EntrepotEmprunts;
 import fr.arpinum.graine.infrastructure.persistance.mongo.EntrepotMongoLinkAvecUuid;
+import org.jongo.Jongo;
 import org.mongolink.MongoSession;
-import org.mongolink.domain.criteria.Criteria;
-import org.mongolink.domain.criteria.Restrictions;
-
-import java.util.List;
 
 public class EntrepotEmpruntMongolink extends EntrepotMongoLinkAvecUuid<Emprunt> implements EntrepotEmprunts{
 
@@ -18,10 +15,10 @@ public class EntrepotEmpruntMongolink extends EntrepotMongoLinkAvecUuid<Emprunt>
 
     @Override
     public boolean existePour(Exemplaire exemplaire) {
-        final Criteria criteria = getSession().createCriteria(Emprunt.class);
-        criteria.add(Restrictions.equals("exemplaire.isbn", exemplaire.isbn()));
-        criteria.add(Restrictions.equals("exemplaire.identifiantBibliotheque", exemplaire.identifiantBibliotheque()));
-        final List<Exemplaire> résultat = criteria.list();
-        return résultat.size() >= 1;
+        final Jongo jongo = new Jongo(getSession().getDb());
+        final long compte = jongo.getCollection("emprunt")
+                .count("{'exemplaire.isbn':#, 'exemplaire.identifiantBibliotheque':#}", exemplaire.isbn(), exemplaire.identifiantBibliotheque());
+        return compte >= 1;
+
     }
 }
