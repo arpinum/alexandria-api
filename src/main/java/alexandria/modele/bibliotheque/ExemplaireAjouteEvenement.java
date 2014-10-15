29afa23cac317573e6424afa695dc070377684c3
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2014-10-15 15:23:34 +0200

    l'ajout de livre est synchrone

--- src/main/java/alexandria/modele/bibliotheque/ExemplaireAjouteEvenement.java
+++ src/main/java/alexandria/modele/bibliotheque/ExemplaireAjouteEvenement.java
@@ -1,9 +1,11 @@
 package alexandria.modele.bibliotheque;
 
 import fr.arpinum.graine.modele.evenement.Evenement;
+import fr.arpinum.graine.modele.evenement.Synchrone;
 
 import java.util.UUID;
 
+@Synchrone
 public class ExemplaireAjouteEvenement implements Evenement {
 
     public ExemplaireAjouteEvenement(String isbn, UUID idBibliotheque) {
