2dd27bce3ccaf86de528a5c23dbcccd0b5b746a5
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2017-06-09 22:28:34 +0200

    refacto saga pour extraire les opérations métiers

--- src/main/java/alexandria/saga/bibliiotheque/AjouteExemplaireBibliothèqueParDéfautSagaCapteur.java
+++ src/main/java/alexandria/saga/bibliiotheque/AjouteExemplaireBibliothèqueParDéfautSagaCapteur.java
@@ -4,13 +4,16 @@ package alexandria.saga.bibliiotheque;
 import alexandria.command.bibliotheque.AjoutExemplaireCommande;
 import alexandria.command.bibliotheque.CreeBibliothequeParDéfautCommande;
 import alexandria.modele.bibliotheque.IdentifiantExemplaire;
+import alexandria.modele.lecteur.Lecteur;
 import alexandria.modele.lecteur.RegistreLecteurs;
 import arpinum.command.CommandBus;
 import arpinum.saga.SagaHandler;
 import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import io.vavr.concurrent.Future;
 
 import javax.inject.Inject;
+import java.util.function.Function;
 
 public class AjouteExemplaireBibliothèqueParDéfautSagaCapteur implements SagaHandler<IdentifiantExemplaire, AjouteExemplaireBibliothèqueParDéfautSaga> {
 
@@ -22,9 +25,17 @@ public class AjouteExemplaireBibliothèqueParDéfautSagaCapteur implements SagaH
     @Override
     public Future<IdentifiantExemplaire> run(CommandBus bus, AjouteExemplaireBibliothèqueParDéfautSaga saga) {
         return registreLecteurs.trouve(saga.idLecteur)
-                .flatMap(lecteur -> bus.send(new CreeBibliothequeParDéfautCommande(lecteur))
-                        .map(id -> Tuple.of(id, lecteur)))
-                .flatMap(t -> t.apply((id, lecteur) -> bus.send(new AjoutExemplaireCommande(id, saga.isbn, lecteur))));
+                .flatMap(créeOuTrouveBibliothèqueParDéfaut(bus))
+                .flatMap(ajouteExemplaire(bus, saga));
+    }
+
+    private Function<Lecteur, Future<? extends Tuple2<String, Lecteur>>> créeOuTrouveBibliothèqueParDéfaut(CommandBus bus) {
+        return lecteur -> bus.send(new CreeBibliothequeParDéfautCommande(lecteur))
+                .map(id -> Tuple.of(id, lecteur));
+    }
+
+    private Function<Tuple2<String, Lecteur>, Future<? extends IdentifiantExemplaire>> ajouteExemplaire(CommandBus bus, AjouteExemplaireBibliothèqueParDéfautSaga saga) {
+        return t -> t.apply((id, lecteur) -> bus.send(new AjoutExemplaireCommande(id, saga.isbn, lecteur)));
     }
 
     private final RegistreLecteurs registreLecteurs;
