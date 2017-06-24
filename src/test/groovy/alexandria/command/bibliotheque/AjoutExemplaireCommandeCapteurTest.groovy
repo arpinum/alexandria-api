b22ec0f3c76018dddb3baae086421082d5cceadf
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2017-06-24 21:55:34 +0200

    test oublié

--- src/test/groovy/alexandria/command/bibliotheque/AjoutExemplaireCommandeCapteurTest.groovy
+++ src/test/groovy/alexandria/command/bibliotheque/AjoutExemplaireCommandeCapteurTest.groovy
@@ -1,9 +1,37 @@
 package alexandria.command.bibliotheque
 
+import alexandria.infrastructure.persistance.memoire.AvecEntrepotsMemoire
+import alexandria.modele.LocalisateurEntrepots
+import alexandria.modele.bibliotheque.Bibliotheque
+import alexandria.modele.lecteur.Lecteur
+import org.junit.Rule
 import spock.lang.Specification
-
 
 class AjoutExemplaireCommandeCapteurTest extends Specification {
 
-    // TODO à écrire
+    @Rule
+    AvecEntrepotsMemoire entrepotsMemoire = new AvecEntrepotsMemoire()
+
+    def "ajoute l'exemplaire"() {
+        given:
+        def lecteur = new Lecteur('idLecteur')
+        def bibliotheque = new Bibliotheque("id", lecteur)
+        LocalisateurEntrepots.bibliotheques().add(bibliotheque)
+        def capteur = new AjoutExemplaireCommandeCapteur()
+
+        when:
+        def résultat = capteur.execute(new AjoutExemplaireCommande('id', 'isbn', lecteur))
+
+        then:
+        résultat != null
+        def idExemplaire = résultat._1
+        idExemplaire != null
+        def évènement = résultat._2.get(0)
+        with(évènement) {
+            targetId == idExemplaire
+            isbn == 'isbn'
+            idLecteur == 'idLecteur'
+            idBibliothèque == 'id'
+        }
+    }
 }
